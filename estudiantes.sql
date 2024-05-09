0b207783f79e1d99164afb1a21b275ecc3ccf088
Mau5trakt <80871251+Mau5trakt@users.noreply.github.com>
2024-05-09 11:21:30 -0600

    Add files via upload

---
+++ estudiantes.sql
@@ -0,0 +1,11 @@
+CREATE TABLE 'estudiantes' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'nombre' TEXT, 'apellido' TEXT, 'nota' INTEGER);
+
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Enrique', 'Alemany', 70);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Jeison', 'Cisneros', 85);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Cintia', 'Duarte', 90);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Hilary', 'Noguera', 82);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Salomón', 'Montiel', 78);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Leonel', 'Real', 72);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Leandro', 'Lacayo', 77);
+INSERT INTO estudiantes (nombre, apellido, nota) VALUES ('Casto', 'Silva', 90);
+
